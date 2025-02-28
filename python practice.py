3a0176ec705faf570ee3388150bd7f29852a0088
Nimaro Patience <161746703+nimaropatience@users.noreply.github.com>
2025-02-28 23:48:14 +0300

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,98 @@
+# print(f"Hello")
+# byear=int(input("Enter year of birth:"))
+# year_now=2024
+# age=(year_now-byear)
+# print(f"The age is{age}")
+
+#TEST CORRECTIONS
+# A=int(input("Enter number one:"))
+# B= int(input("Enter number two:"))
+# print(f"The sum of the numbers is{A+B}")
+# print(f"The product of the numbers is{A *B}")
+
+
+#sring manipulation
+
+# def process_text(text):
+#     # convert the text to lower case
+#     text=text.lower()
+#     print("The text  after converting to lower case is:",text)
+    
+
+#     #removing any leading and trailing white space
+
+#     text=text.strip()
+#     print(f"The text after removing whitespace:{text}")
+
+    
+
+#     #replacing spaces between words with hyphens
+#     text=text.replace(" ","-")
+#     print(f"The text after replacing spaces between words with hyphens is:{text}")
+#     #replacing python with PYTHON
+#     text=text.replace("python","PYTHON")
+#     print(f"The text after replacing python with PYTHON is:{text}")
+#     #reversing the entire sring
+#     text=text[::-1]
+#     print(f"The text after reversing is:{text}")
+#     return text
+# process_text(" Welcome to the Python programming language")
+
+#list manipulation
+
+
+
+#range function
+# for x in range(6):
+#     print(x)
+
+# for x in range(2,8):
+#     print(x)    
+
+
+# for x in range (2,28,2):
+#     print(x)
+
+
+#WHILE LOOP
+# count = 1
+# while count <=5:
+#     print(count)
+#     count += 1
+
+
+# count = 2
+# while count <= 5:
+#     print(count) 
+#     count += 1  
+
+# fruits=["banana","apple","oranges"]
+# for items in fruits:
+#     print(items)
+#     if items =="apple":
+#         break
+
+    
+
+# fruits=["banana","apple","oranges"]
+# for items in fruits:
+#      if items =="apple":
+#          break
+#      print(items)
+
+    
+fruits=["banana","apple","oranges"]
+for items in fruits:
+      if items =="apple":
+          continue
+print(items)
+ 
+    
+
+
+# for x in range(7) :
+#      if (x == 4 ):
+#           continue
+#      print(x)
+
+
